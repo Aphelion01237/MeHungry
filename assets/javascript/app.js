fd32b4591b444104164baf9a401f261bca0f05f4
Aphelion <Aphelion01237@gmail.com>
2019-09-19 12:18:59 -0700

    cleaned up some stuff, trying to get radius selection to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,37 +52,16 @@ function initMap(position) {
           console.log(resturantID);
           console.log(i+1)
           $("#name-"+i).append().text(resturantName)
+          $("#name-"+i).append().attr('resturantID',resturantID)
           $("#price-"+i).text(resturantPrice)
           $("#rating-"+i).text(resturantRating)
           }
       }
   });
 }
+
 $("#radiusButton").click(getLocation)
 
-//     check local storage for unique id key on
-//         if no key is present, continue as normal
-//         if key is present, proceed to navigation page
-
-// query google maps api
-//     get all resturant establishments in both radii
-//     do this sequentially, walking first
-
-// make array of unique keywords for resturants from response
-//     do this FOR EACH RADIUS
-
-// populate dropdown menu with strings from the array
-//     depending on which radius is selected (walking or driving)
-//     of no results, generate modal with sadface and "guess you're gonna starve"
-
-// when input is selected in dropdown menu
-//     query google maps for the specific keyword, within resturant establishments
-//     turn result into an array
-//     sort array based on rating (highest first)
-
-// when GO button is pressed
-//     generate modal with top three [0, 1, 2] from rating sorted array
-//     each result in modal should be a button
 
 // when button in modal is pressed
 //     save place selection information to unique id key in local storage
@@ -104,21 +83,9 @@ $("#radiusButton").click(getLocation)
 //         one button copies address to clipboard
 //         one button resets app, deleting local storage and loading main page
 
-
-
-
-
-
-$("#button").on("click", function() {
-
-$('.large.modal').modal('show');
-
-})
-$(function(){
-	$("#test").click(function(){
-		$(".test").modal('show');
-	});
-	$(".test").modal({
-		closable: true
-	});
-})
+$("#test").click(function(){
+  $(".test").modal('show');
+});
+$(".test").modal({
+  closable: true
+});
